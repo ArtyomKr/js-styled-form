f67a24c2115018bf7688fb44ddc36ff937298955
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-10-29 14:30:34 +0300

    refactor: move server url into const

--- src/components/form/sendForm.js
+++ src/components/form/sendForm.js
@@ -1,5 +1,7 @@
+const url = 'http://localhost:9090/api/registration';
+
 export default function sendForm(form) {
-  return fetch('http://localhost:9090', {
+  return fetch(url, {
     method: 'POST',
     body: new FormData(form),
   });
